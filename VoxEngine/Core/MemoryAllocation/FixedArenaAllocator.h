659a90e7d2d1c99d1a91a3d2e234c9332054cdf0
ShadowBlader <sutuginaleksandr1@gmail.com>
2026-01-14 19:21:11 +0200

    Added memory allocator to make data more dense.

---
+++ VoxEngine/Core/MemoryAllocation/FixedArenaAllocator.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <vector>
+#include <new>
+
+template<typename T, size_t ItemsPerBlock>
+class FixedArenaAllocator
+{
+    static_assert(ItemsPerBlock > 0, "ItemsPerBlock must be greater than 0");
+
+    struct Block
+    {
+        T* memory;
+
+        Block() : memory(static_cast<T*>(std::malloc(sizeof(T) * ItemsPerBlock)))
+        {
+            if (!memory) throw std::bad_alloc();
+        }
+
+        ~Block()
+        {
+            std::free(memory);
+        }
+
+        Block(const Block&) = delete;
+        Block& operator=(const Block&) = delete;
+
+        Block(Block&& other) noexcept : memory(other.memory)
+        {
+            other.memory = nullptr;
+        }
+
+        Block& operator=(Block&& other) noexcept
+        {
+            if (this != &other) {
+                std::free(memory);
+                memory = other.memory;
+                other.memory = nullptr;
+            }
+            return *this;
+        }
+
+        T* getSlot(size_t index) { return &memory[index]; }
+    };
+
+    std::vector<Block> blocks;
+    size_t itemsUsedInCurrentBlock = 0;
+public:
+    FixedArenaAllocator()
+    {
+        allocateBlock();
+    }
+
+    ~FixedArenaAllocator()
+    {
+        // Call destructors for all constructed objects
+        for (size_t blockIdx = 0; blockIdx < blocks.size(); blockIdx++)
+        {
+            Block& block = blocks[blockIdx];
+            size_t itemsInThisBlock = (blockIdx == blocks.size() - 1)
+                ? itemsUsedInCurrentBlock
+                : ItemsPerBlock;
+
+            for (size_t i = 0; i < itemsInThisBlock; i++)
+            {
+                block.getSlot(i)->~T();
+            }
+        }
+    }
+
+    FixedArenaAllocator(const FixedArenaAllocator&) = delete;
+    FixedArenaAllocator& operator=(const FixedArenaAllocator&) = delete;
+
+    FixedArenaAllocator(FixedArenaAllocator&& other) noexcept :
+        blocks(std::move(other.blocks)) ,
+        itemsUsedInCurrentBlock(other.itemsUsedInCurrentBlock)
+    {
+        other.itemsUsedInCurrentBlock = 0;
+    }
+
+    FixedArenaAllocator& operator=(FixedArenaAllocator&& other) noexcept
+    {
+        if (this != &other)
+        {
+            this->~FixedArenaAllocator();
+
+            blocks = std::move(other.blocks);
+            itemsUsedInCurrentBlock = other.itemsUsedInCurrentBlock;
+            other.itemsUsedInCurrentBlock = 0;
+        }
+        return *this;
+    }
+
+    T* allocate()
+    {
+        if (itemsUsedInCurrentBlock >= ItemsPerBlock)
+        {
+            allocateBlock();
+            itemsUsedInCurrentBlock = 0;
+        }
+
+        T* ptr = blocks.back().getSlot(itemsUsedInCurrentBlock++);
+        return ptr;
+    }
+
+    template<typename... Args>
+    T* create(Args&&... args)
+    {
+        T* ptr = allocate();
+        new(ptr) T(std::forward<Args>(args)...);
+        return ptr;
+    }
+
+    void reset()
+    {
+        // Call destructors for all constructed objects
+        for (size_t blockIdx = 0; blockIdx < blocks.size(); blockIdx++)
+        {
+            Block& block = blocks[blockIdx];
+            size_t itemsInThisBlock = (blockIdx == blocks.size() - 1)
+                ? itemsUsedInCurrentBlock
+                : ItemsPerBlock;
+
+            for (size_t i = 0; i < itemsInThisBlock; i++)
+            {
+                block.getSlot(i)->~T();
+            }
+        }
+
+        blocks.clear();
+        allocateBlock();
+        itemsUsedInCurrentBlock = 0;
+    }
+
+    size_t capacity() const { return blocks.size() * ItemsPerBlock; }
+    size_t size() const { return (blocks.size() - 1) * ItemsPerBlock + itemsUsedInCurrentBlock; }
+    size_t itemsPerBlock() const { return ItemsPerBlock; }
+private:
+    void allocateBlock()
+    {
+        blocks.emplace_back();
+    }
+};
